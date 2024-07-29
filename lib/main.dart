33c9173573ce42ff98f4661881b9395854449f06
NihanthT <tatikonda.nihanth@gmail.com>
2024-07-29 14:53:11 -0700

    homepage top bar and import statement in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'homepage_top_bar.dart';
+
 
 void main() {
   runApp(MyApp());
